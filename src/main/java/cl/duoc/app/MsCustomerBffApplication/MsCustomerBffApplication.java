9c656cbdb7c0a77d8f44a59595c3380efd9137e5
wenchispop <debo.palma@duocuc.cl>
2026-03-28 10:00:25 -0300

    Update name artifact
    Update name artifact

--- src/main/java/cl/duoc/app/MsCustomerBffApplication/MsCustomerBffApplication.java
+++ src/main/java/cl/duoc/app/MsCustomerBffApplication/MsCustomerBffApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package cl.duoc.app.MsCustomerBffApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MsCustomerBffApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MsCustomerBffApplication.class, args);
 	}
 
 }
--- src/test/java/cl/duoc/app/MsCustomerBffApplication/MsCustomerBffApplicationTests.java
+++ src/test/java/cl/duoc/app/MsCustomerBffApplication/MsCustomerBffApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package cl.duoc.app.MsCustomerBffApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MsCustomerBffApplicationTests {
 
 	@Test
 	void contextLoads() {
